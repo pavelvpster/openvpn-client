9d401650a4d46cb4799793ae5f41aa507899439c
Pavel Prokhorov <pavelvpster@gmail.com>
2022-10-29 21:00:13 +0600

    Set container DNS

--- start.sh
+++ start.sh
@@ -4,7 +4,9 @@ docker run \
 	--rm \
 	--name openvpn-client \
 	--cap-add=NET_ADMIN \
-	--device /dev/net/tun \
+	--device=/dev/net/tun \
+	--dns=8.8.8.8 \
+	--dns=8.8.4.4 \
 	-it \
 	-p 1080:1080 \
 	-v $(pwd)/config:/home \
